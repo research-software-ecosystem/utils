1fdaeb0113586fd8d7e26433c69f2eb8e60cb7b5
Hervé Ménager <herve.menager@gmail.com>
2021-04-14 17:11:57 +0200

    add `set -e` to `checkout-pr-if-exist.sh`

--- scripts/git-utils/checkout-pr-if-exist.sh
+++ scripts/git-utils/checkout-pr-if-exist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # get all opened PRs  with specified label
+set -e
 RESULT=`curl -s https://api.github.com/search/issues?q=is:pr%20state:open%20label:$1%20repo:bio-tools/content`
 # check if PR is created by github-actions bot and return pull request-number
 PR_NUMBER=`jq '.items[]|select(.user.login == "github-actions[bot]")|.number' <<< $RESULT`
