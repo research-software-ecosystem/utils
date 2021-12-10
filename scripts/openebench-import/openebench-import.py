7561278c7908f9510017f4d17fe44307253d4348
Dmitry Repchevsky <redmitry@list.ru>
2021-12-10 14:37:06 +0100

    print basic stats on changes
    Statistics is calculated on the basement of objects, not files.
    "add: {files added} / {objects added}, del: {objects removed}, dif: ({properties changed / total properties})"

--- scripts/openebench-import/openebench-import.py
+++ scripts/openebench-import/openebench-import.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import json
+import jsonpatch
 import urllib.request
 
 TOOLS_CONTENT_PATH = "../../../content/data/"
 OPENEBENCH_METRICS_ENDPOINT = "https://openebench.bsc.es/monitor/metrics/"
 
-def main():
+JSONPATH_FILTER = ['/@timestamp', '/project/website/last_check', '/project/website/access_time', '/project/website/last_month_access/', '/project/website/half_year_stat/']
 
+def main():
   # dictionary to group metrics by the tool i.e. {'trimal' : [json1, json2, json3]}
   git_metrics = {}
   metrics = get_metrics()
@@ -28,9 +31,47 @@ def main():
       else:
         git_metrics[tool_dir] = [m]
 
+  files_added = 0
+  obj_added = 0
+  obj_removed = 0
+  prop_total = 0
+  prop_changed = 0
+
   for tool_dir, m in git_metrics.items():
-    with open(tool_dir + '/' + os.path.basename(tool_dir) + '.oeb.metrics.json', 'w') as f:
-      json.dump(m, f, indent=4, sort_keys=True)
+    path = tool_dir + '/' + os.path.basename(tool_dir) + '.oeb.metrics.json'
+    if (os.path.isfile(path)):
+      with open(path, 'r+') as f:
+        old = {}
+        try:
+          for old_metrics in json.load(f):
+            old[old_metrics.get('@id')] = old_metrics
+        except Exception as ex:
+          print('invalid json file: ', path, ' ', ex, file = sys.stderr)
+
+        for new_metrics in m:
+          prop_total += traverse(new_metrics)
+
+          _id = new_metrics.get('@id')
+          old_metrics = old.get(_id)
+          if (old_metrics != None):
+            patch = op_filter(jsonpatch.JsonPatch.from_diff(old_metrics, new_metrics).patch);
+            prop_changed += len(patch);
+            old.pop(_id) 
+          else:
+            obj_added += 1
+
+        obj_removed += len(old)
+    else:
+      obj_added += len(m)
+      files_added  += 1
+
+    with open(path, 'w') as f:
+      try:
+        json.dump(m, f)
+      except Exception as ex:
+        print('error writing file: ', path, ' ', ex, file = sys.stderr)
+      
+  print('add:', files_added, '/', obj_added, 'del:', obj_removed, 'dif:', (prop_changed / prop_total))
 
 # Get OpenEBench metrics
 def get_metrics():
@@ -40,6 +81,29 @@ def get_metrics():
     return json.loads(data)
 
   print("error reading metrics", req) 
+
+def op_filter(patch):
+  for i, op in reversed(list(enumerate(patch))):
+    path = op.get('path')
+    for f in JSONPATH_FILTER:
+      if (path.startswith(f)):
+        patch.pop(i)
+        break
+  return patch
+
+# count total number of properties in the (json) object
+def traverse(obj):
+  count = 1
+  if isinstance(obj, dict):
+    count = len(obj)  
+    for val in obj.values():
+      count += traverse(val)
+  elif isinstance(obj, list):
+    count = len(obj) 
+    for val in obj:
+      count += traverse(val)
+
+  return count
 
 if __name__ == "__main__":
     main()
