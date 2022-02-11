71d1b33b5011335a14d86b25a55d4eb67e31bafb
Dmitry Repchevsky <redmitry@list.ru>
2022-02-11 13:52:54 +0100

    Create comment-pr.sh
    script to set the commit message on the pull request.
    consequent calls overwrite the previous message.
    usage:
    ~~~
    ${{ github.workspace }}/content-ecosystem-utils/scripts/git-utils/comment-pr.sh openebench-import "comment message"
    ~~~

---
+++ scripts/git-utils/comment-pr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# looking for the Pull Request by the label ($1) and put the comment on it ($2)
+
+for i in {1..20}
+do
+    RESULT=`curl -s https://api.github.com/search/issues?q=is:pr%20state:open%20label:$1%20repo:bio-tools/content`
+    COMMENTS_URL=`jq -r '.items[]|select(.user.login == "github-actions[bot]")|.pull_request.url' <<< "$RESULT"`
+    if [ ! -z "$COMMENTS_URL" ]; then
+	    curl -s -u "$GITHUB_USER":"$GITHUB_TOKEN" -H 'Content-Type: application/json' -L "$COMMENTS_URL" -d '{"body": "'"$2"'"}'
+        break
+    fi
+    sleep ${i}   # wait for pull request to be visualized
+done
