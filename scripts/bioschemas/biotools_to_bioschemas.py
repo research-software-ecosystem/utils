b89e3be3d5705c80aaa3a3cb00be60ffd7fa4a06
Hervé Ménager <herve.menager@pasteur.fr>
2022-10-19 22:30:27 +0200

    correct path to bioschemas files which should be removed

--- scripts/bioschemas/biotools_to_bioschemas.py
+++ scripts/bioschemas/biotools_to_bioschemas.py
@@ -361,7 +361,8 @@ def process_tools_by_id(id="SPROUT"):
                 print(f"generated markup at {dest}")
 
 def clean():
-    for data_file in glob.glob(r"data/*/*.bioschemas.jsonld"):
+    for data_file in glob.glob(r"../../../content/data/*/*.bioschemas.jsonld"):
+        print(f"removing file {data_file}")
         os.remove(data_file)
 
 def process_tools():
