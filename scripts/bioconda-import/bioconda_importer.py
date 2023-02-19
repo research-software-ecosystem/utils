8cda498b88fbb43bc0ae8e84f98537751bde8618
Hervé Ménager <herve.menager@pasteur.fr>
2023-02-19 11:06:54 +0100

    fix bioconda import script
    - remove pickle caching code
    - add requirements file
    - code gardening

--- scripts/bioconda-import/bioconda_importer.py
+++ scripts/bioconda-import/bioconda_importer.py
@@ -4,7 +4,6 @@ import json
 import os
 import yaml
 
-import pickle
 import argparse
 from pathlib import Path
 
@@ -23,15 +22,13 @@ def parse_biotools(directory):
     """
     data = dict()
     for root, dirs, files in os.walk(directory):
-        excluded = ["raw.json", "oeb.json", "biocontainers.json"]
         for filename in files:
-            if not filename.endswith(tuple(excluded)) and filename.endswith(".json"):
+            if filename.endswith("biotools.json"):
                 filepath = os.path.join(root, filename)
-                with open(filepath, 'r') as f:
+                with open(filepath, "r") as f:
                     biotools = json.load(f)
-                    # print(biotools)
-                    bio_id = biotools['biotoolsID'].lower()
-                    data[bio_id] = {'data': biotools, 'path': filepath}
+                    bio_id = biotools["biotoolsID"].lower()
+                    data[bio_id] = {"data": biotools, "path": filepath}
     return data
 
 
@@ -39,76 +36,67 @@ def parse_bioconda(directory):
     """
     Function to get bioconda content data into memory.
     """
-
     data = dict()
-    # print(Path(directory).glob('./*/meta.yaml'))
-    for p in Path(directory).glob('./*/meta.yaml'):
-        # print(p.absolute())
+    for p in Path(directory).glob("./*/meta.yaml"):
         template = jinja2.Template(p.read_text())
-        # print(template.render())
-        conda = yaml.safe_load(template.render(
-            {'os': os, 'compiler': fake, 'environ': '', 'cdt': fake, 'pin_compatible': fake, 'pin_subpackage': fake,
-             'exact': fake}))
-        extras = conda.get('extra', None)
-        # if conda['package']['name'].strip() != 'deeptools':
-        #    continue
+        conda = yaml.safe_load(
+            template.render(
+                {
+                    "os": os,
+                    "compiler": fake,
+                    "environ": "",
+                    "cdt": fake,
+                    "pin_compatible": fake,
+                    "pin_subpackage": fake,
+                    "exact": fake,
+                }
+            )
+        )
+        extras = conda.get("extra", None)
         identifiers = defaultdict(list)
         if extras:
-            ids = extras.get('identifiers', None)
-            # print(ids)
+            ids = extras.get("identifiers", None)
             if ids:
                 for id in ids:
-                    n, c = id.split(':', 1)
+                    n, c = id.split(":", 1)
                     identifiers[n].append(c)
-        data[str(p.absolute())] = dict({'recipe': conda, 'ids': identifiers})
-        # print(conda['package']['name'], str(identifiers))
+        data[str(p.absolute())] = dict({"recipe": conda, "ids": identifiers})
 
     return data
-    """
-
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
-            if filename == "meta.yaml":
-                filepath = os.path.join(root, filename)
-                with open(filepath, 'r') as f:
-                    conda = yaml.load(Environment().from_string(content).render())
-                    ##conda = yaml.load(f)
-                    print(conda['package']['name'])
-                    data[filepath] = conda
-    """
-    # return data
 
 
 def create_metadata(conda, path, biotools_id):
-    data = conda['recipe']['package']
-    data.update(conda['recipe']['about'])
-    extra = conda['recipe'].get('extra', None)
+    data = conda["recipe"]["package"]
+    data.update(conda["recipe"]["about"])
+    extra = conda["recipe"].get("extra", None)
     if extra:
-        identifiers = extra.get('identifiers', None)
-        if extra.get('identifiers', None):
-            data.update({'identifiers': extra['identifiers']})
-    data.update({'biotools_id': biotools_id})
-    with open(path, 'w') as out:
+        identifiers = extra.get("identifiers", None)
+        if extra.get("identifiers", None):
+            data.update({"identifiers": extra["identifiers"]})
+    data.update({"biotools_id": biotools_id})
+        print(f"updating {path}...")
+        print(data)
         yaml.dump(data, out)
 
 
 def merge(tools, conda, content_path):
     for name, data in conda.items():
-        ids = data['ids']
+        ids = data["ids"]
         if ids:
-            bio = ids.get('biotools', None)
+            bio = ids.get("biotools", None)
             if bio:
                 # fix me ... recipes with multiple biotools, ids
                 # assert len(bio) == 1
                 bio = bio[0].lower()
                 path = os.path.join(content_path, bio)
+                print(f"bioconda file {path} bio.tools entry {bio}")
                 if not tools.get(bio, None):
-                    print('None bio.tools entry', bio)
+                    print("None bio.tools entry", bio)
                     # if not os.path.exists(path):
                     #    os.mkdir(path)
                     # create_metadata(data, '%s/bioconda_%s.yaml' % (path, bio), bio)
                     continue
-                create_metadata(data, '%s/bioconda_%s.yaml' % (path, bio), bio)
+                create_metadata(data, "%s/bioconda_%s.yaml" % (path, bio), bio)
                 # print(bio, name, tools[bio]['path'])
                 continue
 
@@ -117,31 +105,33 @@ class readable_dir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         prospective_dir = values
         if not os.path.isdir(prospective_dir):
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
+            raise argparse.ArgumentTypeError(
+                "readable_dir:{0} is not a valid path".format(prospective_dir)
+            )
         if os.access(prospective_dir, os.R_OK):
             setattr(namespace, self.dest, prospective_dir)
         else:
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
+            raise argparse.ArgumentTypeError(
+                "readable_dir:{0} is not a readable dir".format(prospective_dir)
+            )
 
 
-parser = argparse.ArgumentParser(description='test', fromfile_prefix_chars="@")
-parser.add_argument("biotools", help="path to metadata dir, e.g. content/data/", type=str, action=readable_dir)
-parser.add_argument("bioconda", help="path to bioconda recipes, e.g. bioconda-recipes/recipes", type=str,
-                    action=readable_dir)
-args = parser.parse_args()
-print(args)
-if __name__ == '__main__':
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="test", fromfile_prefix_chars="@")
+    parser.add_argument(
+        "biotools",
+        help="path to metadata dir, e.g. content/data/",
+        type=str,
+        action=readable_dir,
+    )
+    parser.add_argument(
+        "bioconda",
+        help="path to bioconda recipes, e.g. bioconda-recipes/recipes",
+        type=str,
+        action=readable_dir,
+    )
+    args = parser.parse_args()
     content_path = args.biotools
-
-    if not os.path.exists('./tools_data.pkl'):
-        tools = parse_biotools(args.biotools)
-        pickle.dump(tools, open("./tools_data.pkl", "wb"))
-    else:
-        tools = pickle.load(open('./tools_data.pkl', 'rb'))
-    if not os.path.exists('./conda_data.pkl'):
-        conda = parse_bioconda(args.bioconda)
-        pickle.dump(conda, open("./conda_data.pkl", "wb"))
-    else:
-        conda = pickle.load(open('./conda_data.pkl', 'rb'))
-
+    tools = parse_biotools(args.biotools)
+    conda = parse_bioconda(args.bioconda)
     merge(tools, conda, content_path)
