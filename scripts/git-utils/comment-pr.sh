c23bfa09a9d02a45cedca694e5ebc7b9906354d4
Hervé Ménager <herve.menager@pasteur.fr>
2023-05-09 23:36:11 +0200

    change target repository for comment-pr.sh

--- scripts/git-utils/comment-pr.sh
+++ scripts/git-utils/comment-pr.sh
@@ -3,7 +3,7 @@
 
 for i in {1..10}
 do
-    RESULT="$(curl -s https://api.github.com/search/issues?q=is:pr%20state:open%20label:$1%20repo:bio-tools/content)"
+    RESULT="$(curl -s https://api.github.com/search/issues?q=is:pr%20state:open%20label:$1%20repo:research-software-ecosystem/content)"
     COMMENTS_URL="$(jq -r '.items[]|select(.user.login == "github-actions[bot]")|.pull_request.url' <<< "$RESULT")"
     if [ ! -z "$COMMENTS_URL" ]; then
 	    curl -s -u "$GITHUB_USER":"$GITHUB_TOKEN" -H 'Content-Type: application/json' -L "$COMMENTS_URL" -d '{"body": "'"$2"'"}'
