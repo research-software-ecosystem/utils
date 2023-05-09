0fee268929c9b8564a44cf04d0c72413d6a24d7b
Hervé Ménager <herve.menager@pasteur.fr>
2023-05-09 23:35:39 +0200

    change target repository for checkout-pr-if-exist.sh

--- scripts/git-utils/checkout-pr-if-exist.sh
+++ scripts/git-utils/checkout-pr-if-exist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 # get all opened PRs with specified label
-RESULT=`curl -s https://api.github.com/search/issues?q=is:pr%20state:open%20label:$1%20repo:bio-tools/content`
+RESULT=`curl -s https://api.github.com/search/issues?q=is:pr%20state:open%20label:$1%20repo:research-software-ecosystem/content`
 # check if PR is created by github-actions bot and return pull request-number
 PR_NUMBER=`jq '.items[]|select(.user.login == "github-actions[bot]")|.number' <<< $RESULT`
 # get the branch name and remove quotes
-TARGET_BRANCHNAME=`jq '.head.ref' <<< $(curl -s curl -s https://api.github.com/repos/bio-tools/content/pulls/$PR_NUMBER) | sed -r 's/^"|"$//g'`
+TARGET_BRANCHNAME=`jq '.head.ref' <<< $(curl -s curl -s https://api.github.com/repos/research-software-ecosystem/content/pulls/$PR_NUMBER) | sed -r 's/^"|"$//g'`
 echo "PR number: $PR_NUMBER"
 echo "branch name: $TARGET_BRANCHNAME"
 
