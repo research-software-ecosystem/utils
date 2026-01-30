8dd971653f7be0419eb5d8e62b69872e4503b0ba
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2026-01-30 17:34:50 +0100

    save file list for biotools dump

--- bioschemas-gen/biotools_bioschemas_dump.py
+++ bioschemas-gen/biotools_bioschemas_dump.py
@@ -11,6 +11,9 @@ def get_bioschemas_files_in_repo():
         if len(filename_ext) == 3 and filename_ext[2] == "jsonld":
             tools.append(data_file)
     print(f"found {len(tools)} bioschemas descriptors")
+    with open("../../content/datasets/biotools_bioschemas_files_list.txt", "w", encoding="utf-8") as f:
+        for tool in tools:
+            f.write(f"{tool}\n")
     return tools
 
 
